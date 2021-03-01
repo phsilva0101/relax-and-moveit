1704cc6079d87990847ab28a52b03a43bc827d41
Paulo <paulohsilva2001@gmail.com>
2021-03-01 20:11:51 -0300

    Criando context API do react, para o Countdown

---
+++ src/contexts/CountdownContext.tsx
@@ -0,0 +1,78 @@
+import { Children, createContext, ReactNode, useContext, useEffect, useState} from 'react'
+import { ChallengeContext } from './ChallengeContext'
+
+interface CountdownContextData {
+    minutes: number;
+    seconds:number;
+    hasBeenFinished: boolean;
+    isActive: boolean;
+    startCountdown: () => void;
+    resetCountdown: () => void;
+}
+
+interface CountdownProviderProps {
+    children: ReactNode
+}
+
+let countdownTimeOut: NodeJS.Timeout; /* -> Tipagem global */
+
+
+export const CountdownContext = createContext ({} as CountdownContextData)
+
+export function CountdownProvider( {children} : CountdownProviderProps){
+
+    const { startNewChallenge } = useContext(ChallengeContext)
+
+
+    const [time, setTime] = useState(25 * 60)
+    const [isActive, setIsActive] = useState(false)
+    const [hasBeenFinished, setHasBeenFinished] = useState (false)
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60
+
+    /*---------------- Funções ---------------*/
+    function startCountdown(){
+        setIsActive(true)
+    }
+
+    function resetCountdown(){
+        clearTimeout(countdownTimeOut)
+        setIsActive(false)
+        setHasBeenFinished(false)
+        setTime(25 * 60)
+            
+    }
+
+
+    /*--------------------useEffect ---------------*/
+
+    useEffect (() => {
+        if(isActive && time > 0){
+          countdownTimeOut = setTimeout(() => {
+                setTime(time - 1)
+            }, 1000);
+        } else if(isActive && time === 0){        
+            setHasBeenFinished(true)
+            setIsActive(false)
+            startNewChallenge()
+        }
+    }, [isActive, time])
+
+    return(
+        <CountdownContext.Provider
+        value={{
+            minutes,
+            seconds,
+            hasBeenFinished,
+            isActive,
+            startCountdown,
+            resetCountdown
+
+
+        }}
+        >
+            {children}
+        </CountdownContext.Provider>
+    )
+}

a18aff163a6def2b1b1e1b9098e9e28f6faedfb9
Paulo <paulohsilva2001@gmail.com>
2021-03-01 20:10:11 -0300

    Caixa de desafios

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react'
+import { ChallengeContext } from '../contexts/ChallengeContext'
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+export function CompletedChallenges(){
+ const {challengeCompleted} = useContext(ChallengeContext)
+
+    return(
+        
+            <div className={styles.challengesContainer}>
+                <span>Desafios completos</span>
+                <span>{challengeCompleted}</span>
+
+            </div>
+           
+            
+           
+        
+        
+    )
+}
